cf861efa43421e3e2f6a8ca218ad93ff0e7c8919
RafaDev-gft <rlmy@gft.com>
2025-11-19 01:30:15 -0500

    feat: Add Tailwind CSS configuration and custom styles; include autoprefixer and postcss dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
- /** @type {import('tailwindcss').Config} */
-export default {
-   content: ["./src/**/*.{html,js,tsx,ts}"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'design-primary-100': 'var(--color-design-primary-100)',
+        'design-primary-300': 'var(--color-design-primary-300)',
+        'design-primary-500': 'var(--color-design-primary-500)',
+        'design-primary-700': 'var(--color-design-primary-700)',
+        'design-primary-900': 'var(--color-design-primary-900)',
+        'design-neutral-50': 'var(--color-design-neutral-50)',
+        'design-neutral-100': 'var(--color-design-neutral-100)',
+        'design-neutral-300': 'var(--color-design-neutral-300)',
+        'design-neutral-400': 'var(--color-design-neutral-400)',
+        'design-neutral-600': 'var(--color-design-neutral-600)',
+        'design-neutral-800': 'var(--color-design-neutral-800)',
+      },
+    },
+  },
+  plugins: [],
+}
